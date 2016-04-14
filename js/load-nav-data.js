e9e444b131e18bfed6087ba5772e9a10af92c1be
harfordt <tim.harford@wgc.school.nz>
2016-04-15 10:12:35 +1200

    Updated home links, description on home page

--- js/load-nav-data.js
+++ js/load-nav-data.js
@@ -1,4 +1,4 @@
-var $navhtml = '<li><a href="#">Home</a></li>\
+var $navhtml = '<li><a href="index.html">Home</a></li>\
           <li class="dropdown">\
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Modals<span class="caret"></span></a>\
             <ul class="dropdown-menu">\
