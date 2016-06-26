a0067ac5ed1ecad01b1f85a2ab6b7df0fb534f49
harfordt <tim.harford@wgc.school.nz>
2016-06-26 14:00:10 +1200

    Move and add new sites
    Move Annisha web folder to root level, delete markscheme.
    
    Add Maria and Alice F.

---
+++ 91635/takeaway/js/image.js
@@ -0,0 +1,8 @@
+$('#colorboxExample').justifiedGallery({
+    //    lastRow: 'nojustify',
+    rowHeight: 100,
+    rel: 'gallery1', //replace with 'gallery1' the rel attribute of each link
+    margins: 1
+});
+
+$('a.gallery').colorbox();
