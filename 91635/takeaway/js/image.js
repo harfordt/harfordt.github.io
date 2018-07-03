e002ef6c71cba8d8f3f8979f1a5d584253565d59
harfordt <tim.harford@wgc.school.nz>
2018-07-03 20:16:50 +1200

    Removed redundant and obsolete files

--- 91635/takeaway/js/image.js
+++
@@ -1,8 +0,0 @@
-$('#colorboxExample').justifiedGallery({
-    //    lastRow: 'nojustify',
-    rowHeight: 100,
-    rel: 'gallery1', //replace with 'gallery1' the rel attribute of each link
-    margins: 1
-});
-
-$('a.gallery').colorbox();
